0ec852b21016f2909e889b653c0e03d8ef796826
Elliot Maude <maude.elliot@gmail.com>
2020-08-03 14:58:09 -0400

    LED Module moved out of index

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-var Gpio = require('onoff').Gpio;
+
+const led = require("led");
+
+
 
 var port = 4040;
-
-
 var address, os = require('os') ,ifaces = os.networkInterfaces();
 
 // Iterate over interfaces ...
@@ -36,6 +37,7 @@ app.get('/soilMoisture', (req,res)=>{
 });
 
 let server = app.listen(port, ()=>{
+    led.start();
     console.log(`Server running at http://${address}:${port}/`);
 })
 
@@ -46,23 +48,3 @@ let server = app.listen(port, ()=>{
 */
 
 
-//LED TEST CODE
-
-var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
-var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
-
-function blinkLED() { //function to start blinking
-  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
-  }
-}
-
-function endBlink() { //function to stop blinking
-  clearInterval(blinkInterval); // Stop blink intervals
-  LED.writeSync(0); // Turn LED off
-  //LED.unexport(); // Unexport GPIO to free resources
-}
-
-setTimeout(endBlink, 5000); //stop blinking after 5 seconds 
